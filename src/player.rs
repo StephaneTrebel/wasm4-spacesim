4a777c70f5b77d0ec450cf4ba872c747f13c52b2
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2023-04-13 14:08:33 +0200

    Add inventory in player ship structure

--- src/player.rs
+++ src/player.rs
@@ -1,9 +1,19 @@
-use crate::{maths::Coordinates3d, utils::clamp};
+use core::i32::MAX;
 
-#[derive(Default, Clone, Copy)]
+use hashbrown::HashMap;
+
+use crate::{items::Item, maths::Coordinates3d, utils::clamp};
+
+#[derive(Clone, Eq, Hash, PartialEq)]
+pub struct Inventory {
+    quantity: u32,
+}
+
+#[derive(Default, Clone)]
 pub struct PlayerShip {
     pub position: Coordinates3d,
     pub speed: i32,
+    pub inventory: HashMap<Item, Inventory>,
 }
 
 const MAX_SPEED: i32 = 500;
@@ -18,13 +28,27 @@ impl PlayerShip {
                 w: 1.0,
             },
             speed: 100,
+            inventory: HashMap::new(),
         }
     }
 
     pub fn increment_speed(&mut self) {
         self.speed = clamp(0, self.speed + 1, MAX_SPEED);
     }
+
     pub fn decrement_speed(&mut self) {
         self.speed = clamp(0, self.speed - 1, MAX_SPEED);
+    }
+
+    /// Add item to player ship inventory
+    /// If the item already exist it is overwritten and the previous value is returned
+    pub fn add_item_to_inventory(&mut self, item: &Item, quantity: u32) -> Option<Inventory> {
+        self.inventory.insert(item.clone(), Inventory { quantity })
+    }
+
+    /// Remove item from player ship inventory
+    /// If the item exist it is removed and the previous value is returned
+    pub fn remove_item_from_inventory(&mut self, item: &Item) -> Option<Inventory> {
+        self.inventory.remove(item)
     }
 }
